626de46632cdbfc86f39fc9c791ae4c98f533187
EgorS <egor.sima.2003@gmail.com>
2021-03-06 22:13:42 +0300

    №2

--- Lab2/№2/Program.cs
+++ Lab2/№2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab2
 {
@@ -10,6 +6,22 @@ namespace Lab2
   {
     static void Main(string[] args)
     {
+      ulong result = 0;
+      ulong a = (ulong)Convert.ToUInt64(Console.ReadLine());
+      ulong b = (ulong)Convert.ToUInt64(Console.ReadLine());
+      ulong p = 2;
+      while (p <= b && p != 0)
+      {
+        result += b / p;
+        p *= 2;
+      }
+      p = 2;
+      while (p <= a && p != 0)
+      {
+        result -= a / p;
+        p *= 2;
+      }
+      Console.WriteLine(result);
     }
   }
 }
