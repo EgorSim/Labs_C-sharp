40b7fe595211233ac365551af69858c5eef7ca17
EgorS <egor.sima.2003@gmail.com>
2021-03-06 18:47:45 +0300

    First tast

---
+++ Lab2/№1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab2
+{
+  static class ReverseStr
+  {
+    public static void DisplayRevStr(string str, int index)
+    {
+      if (index >= str.Length) return;
+      int i;
+      for (i = index; i < str.Length && str[i] != ' '; i++) ;
+      DisplayRevStr(str, i + 1);
+      Console.Write(str.Substring(index, i - index) + " ");
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string str = Console.ReadLine();
+      Console.WriteLine();
+      ReverseStr.DisplayRevStr(str, 0);
+      Console.WriteLine();
+    }
+  }
+}
