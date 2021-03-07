09f609dc7e32ba5dc36bcd87f55426c6a18a7a7f
EgorS <egor.sima.2003@gmail.com>
2021-03-07 11:24:06 +0300

    Third task

--- Lab2/№3/Program.cs
+++ Lab2/№3/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _3
+namespace Lab2
 {
   class Program
   {
     static void Main(string[] args)
     {
+      string region;
+      while (true)
+      {
+        Console.Write("Write region(ENTER to exit) : ");
+        region = Console.ReadLine();
+        if (region != "")
+        {
+          System.Globalization.CultureInfo Culture = new System.Globalization.CultureInfo("RU");
+          try
+          {
+            Culture = new System.Globalization.CultureInfo(region);
+          }
+          catch (Exception ex)
+          {
+            Console.WriteLine($"Error: {ex.Message}");
+            continue;
+          }
+          DateTime date = new DateTime(2021, 1, 1, 00, 00, 00);
+          for (int i = 0; i < 12; i++)
+          {
+            Console.WriteLine(date.ToString("MMMM", Culture.DateTimeFormat));
+            date = date.AddMonths(1);
+          }
+          Console.WriteLine();
+        }
+        else
+        {
+          break;
+        }
+      }      
     }
   }
 }
