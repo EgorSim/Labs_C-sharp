f0bc5ba716c1037735d08a93093a3d5e1088cfd7
EgorS <egor.sima.2003@gmail.com>
2021-04-16 14:47:54 +0300

    test

--- Lab7/Program.cs
+++ Lab7/Program.cs
@@ -6,16 +6,31 @@ namespace Lab7
   {
     static void Main(string[] args)
     {
-      RationalNum a = new RationalNum("0.(3)");
-      RationalNum b = new RationalNum("1/3");
-      RationalNum res = a / b;
+      RationalNum a1 = new RationalNum("0.(3)");
+      RationalNum b1 = new RationalNum("1/3");
+      RationalNum res1 = a1 + b1;
+      Console.WriteLine(res1);
+      Console.WriteLine(a1);
+      Console.WriteLine(b1);
+      Console.WriteLine(a1.ToString());
+      Console.WriteLine(b1.ToString());
 
-      float x = a;
-      int y = (int)a;
+      Console.WriteLine();
 
-      Console.WriteLine(a);
-      Console.WriteLine(x);
-      Console.WriteLine(res.ToString());
+
+      RationalNum a2 = new RationalNum("0.654(32)");
+      RationalNum b2 = new RationalNum("-36.987");
+      RationalNum res2 = a2 - b2;
+      Console.WriteLine(res2);
+      Console.WriteLine(a2);
+      Console.WriteLine(b2);
+      Console.WriteLine(a2.ToString());
+      Console.WriteLine(b2.ToString());
+
+
+
+
+
 
     }
   }
